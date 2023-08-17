318269ec3024706c89e822cf16bd753e86554c31
aditya-524 <aditya524@hotmail.com>
2023-08-17 15:16:38 +0930

    MLU01 Proj Initial Commit
    Creation of Physical environment in Unity with scripts for agent, target, background and walls.

---
+++ ML_Unity/MLU01/Assets/Scripts/MoveToTarget.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+/// <summary>
+///
+/// </summary>
+public class MoveToTarget : Agent
+{
+    [SerializeField] private Transform target;
+
+
+    public override void OnEpisodeBegin()
+    {
+        transform.position = new Vector3(Random.Range(-6.4f,-3.5f), Random.Range(-6f,2.5f));
+        target.position = new Vector3(Random.Range(1.5f,4f), Random.Range(-3.5f,3.5f));
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation((Vector2)transform.position);
+        sensor.AddObservation((Vector2)target.position);
+
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveY = actions.ContinuousActions[1];
+        
+        float movementSpeed = 5f;
+
+        transform.localPosition += new Vector3(moveX, moveY) * Time.deltaTime * movementSpeed;
+
+    }
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Target target))
+        {
+            AddReward(10f);
+            EndEpisode();
+        }
+        else if (collision.TryGetComponent(out Wall wall))
+        {
+            AddReward(-2f);
+            EndEpisode();
+        }
+    }
+}
